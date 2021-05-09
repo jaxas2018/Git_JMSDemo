b292aaa773ee6bce8a0a30d57b66285464263788
jaxas <jaxas2018@gmail.com>
2021-05-08 23:43:50 -0400

    The first commit for JMSDemo

---
+++ src/main/java/com/davidx/MessageSender.java
@@ -0,0 +1,43 @@
+package com.davidx;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+ 
+
+public class MessageSender {
+	
+	private static String url=ActiveMQConnection.DEFAULT_BROKER_URL;
+	
+	private static String queueName="MESSAGE_QUEUE";
+
+	public static void main(String[] args) throws JMSException {
+		// TODO Auto-generated method stub
+		System.out.println("url = "+url);
+		
+		ConnectionFactory connectionFactory=new ActiveMQConnectionFactory(url);
+		Connection connection=connectionFactory.createConnection();
+		
+		connection.start();
+		Session session=connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+		Destination destination=session.createQueue(queueName);
+		
+		MessageProducer producer=session.createProducer(destination);
+		
+		TextMessage message=session.createTextMessage("JMS sample sender!");
+		
+		producer.send(message);
+		
+		System.out.println("Message "+message.getText()+", send successful to the queue!");
+		
+		connection.close();
+	}
+
+}
